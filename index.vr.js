eb890514bfe1027b1c93af01e48bbb2440934b34
Tony Spiro <tspiro@tonyspiro.com>
2018-05-06 18:30:52 -0500

    panos edits

--- index.vr.js
+++ index.vr.js
@@ -8,73 +8,60 @@ import {
   Image,
   VrButton
 } from 'react-vr';
+import _ from 'lodash'
 import Cosmic from 'cosmicjs'
+const api = Cosmic()
+const bucket = api.bucket({
+  slug: 'art'
+})
 
 export default class WelcomeToVR extends React.Component {
   constructor() {
     super()
     this.state = {}
-    Cosmic.getObjectsByType({ bucket: { slug: 'art' }}, { type_slug: 'paintings' }, (err, res) => {
-      this.setState({ cosmic: res })
+    bucket.getObjects().then(data => {
+      const cosmic_objects = data.objects
+      const paintings = _.filter(cosmic_objects, { type_slug: 'paintings' })
+      const globals = _.keyBy(_.filter(cosmic_objects, { type_slug: 'globals' }), 'slug')
+      console.log(globals.realm.metadata.pano)
+      this.setState({ 
+        data: {
+          globals,
+          paintings
+        }
+      })
+    }).catch(data => {
+      throw(err)
     })
-  }
-  handlePaintingClick(painting) {
-    this.setState({ ...this.state, content: painting.content })
   }
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano source={asset(`panos/${this.state.data ? this.state.data.globals.realm.metadata.pano + '.jpg' : '' }`)}/>
         {
-          this.state.cosmic && this.state.cosmic.objects.all &&
-          [
-            <VrButton
-              key={ 1 }
-              style={{width: 0.7}}
-              onClick={ ()=> this.handlePaintingClick(this.state.cosmic.objects.all[0])}>
-              <Image
-                source={{ uri: this.state.cosmic.objects.all[0].metadata.painting.imgix_url }}
-                style={{
-                  position: 'absolute',
-                  layoutOrigin: [0.5, .5],
-                  height: 100,
-                  width: 160,
-                  transform: [{translate: [0, 0, -300]}],
-                }}
-              />
-            </VrButton>,
-            <VrButton
-              key={ 2 }
-              style={{width: 0.7}}
-              onClick={ ()=> this.handlePaintingClick(this.state.cosmic.objects.all[1])}>
-              <Image
-                source={{ uri: this.state.cosmic.objects.all[1].metadata.painting.imgix_url }}
-                style={{
-                  position: 'absolute',
-                  layoutOrigin: [0.5, .7],
-                  height: 200,
-                  width: 350,
-                  transform: [{translate: [0, 0, 950]}],
-                }}
-              />
-            </VrButton>
-          ]
-        }
-        {
-          this.state.content &&
-          <Text
-            style={{
-              backgroundColor: '#777879',
-              fontSize: 0.8,
-              layoutOrigin: [0.5, 0.5],
-              paddingLeft: 0.2,
-              paddingRight: 0.2,
-              textAlign: 'center',
-              textAlignVertical: 'center',
-              transform: [{translate: [0, 0, -3]}],
-            }}
-            dangerouslySetInnerHTML={{ __html: this.state.content }}>
-          </Text>
+          this.state.data && this.state.data.paintings.length &&
+          <View> 
+            <Image
+              source={{ uri: this.state.data.paintings[0].metadata.painting.imgix_url }}
+              style={{
+                position: 'absolute',
+                layoutOrigin: [0.5, .5],
+                height: 100,
+                width: 160,
+                transform: [{translate: [0, 0, -300]}],
+              }}
+            />
+            <Image
+              source={{ uri: this.state.data.paintings[1].metadata.painting.imgix_url }}
+              style={{
+                position: 'absolute',
+                layoutOrigin: [0.5, .7],
+                height: 200,
+                width: 350,
+                transform: [{translate: [0, 0, 950]}],
+              }}
+            />
+          </View>
         }
       </View>
     );
